283e61cc025858a91c7bca7a858b9fc70ea046a0
Ankit <uankit53@outlook.com>
2021-10-15 15:45:31 +0530

    Created Single Product Page Design

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import HomePage from "./pages/HomePage";
 import ProductListPage from "./pages/ProductListPage";
+import SingleProductPage from "./pages/SingleProductPage";
 
 export default function App() {
-  return <ProductListPage />;
+  return <SingleProductPage />;
 }
